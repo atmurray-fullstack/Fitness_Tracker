a176c83a289d01b5d72bfa5f27b6590834eecd94
Andrew Murray <atmurray@bellsouth.net>
2020-06-29 15:15:05 -0400

    added function for getting a weeks worth of workout data and started writing api-route for updating workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,34 @@
-const { db } = require("../models/Workout");
+const db = require("../models");
 
 module.exports = (app) => {
 
-
+    ///////////////////  GETS   /////////////////////////////////
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(data => { res.json(data) })
+        db.Workout.find({}).then(data => {
+            console.log(data)
+            res.json(data)
+        })
     });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).sort({ "_id": -1 }).limit(7)
+            ////I have set a limit of 7 because the stats.js file shows 7 day graphs
+            .then(data => {
+                console.log(data);
+                res.json(data);
+            })
+    })
+
+    ////////////////////PUTS//////////////////////////////////
+    app.put("/api/workouts/:id", (res, req) => {
+        db.Workout.findOneAndUpdate(
+            {
+                _id: req.params._id
+            },
+            {
+                "$push": { exercises: req.body }
+            }
+        )
+    });
+
 };
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
@@ -18,8 +19,3 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
-
-const path = require("path");
-// app.get("/:string", (req, res) => {
-//     req.params.string === "exercise" ? res.sendFile(path.join(__dirname, "/public/exercise.html")) : res.sendFile(path.join(__dirname, "/public/index.html"))
-// })
